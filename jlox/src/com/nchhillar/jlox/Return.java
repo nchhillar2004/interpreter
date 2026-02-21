5322e54d650aa6b94213eab868ecbf499e6e5c48
Nishant Chhillar <nchhillar2004@gmail.com>
2026-02-21 19:49:05 +0530

    Functions

---
+++ jlox/src/com/nchhillar/jlox/Return.java
@@ -0,0 +1,10 @@
+package com.nchhillar.jlox;
+
+class Return extends RuntimeException {
+    final Object value;
+
+    Return(Object value) {
+        super(null, null, false, false);
+        this.value = value;
+    }
+}
